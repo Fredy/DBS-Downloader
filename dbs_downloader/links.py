f84c568ee284884d624c107b260a775586e3f120
Fredy Álvarez <fre.alvarezb@gmail.com>
2017-07-25 01:30:43 -0500

    -Write in a file functions

--- dbs_downloader/links.py
+++ dbs_downloader/links.py
@@ -40,3 +40,14 @@ def decript_url(crip_url):
     soup = BeautifulSoup(page.content, "html.parser")
     url = soup.find(text=True)
     return url
+
+def write_file_single(eps_num, file_name):
+    with open(file_name, 'w') as f :
+        res = single_eps(eps_num)
+        f.write(res)
+
+def write_file_range(start, end, file_name):
+    res = range_eps(start, end)
+    with open(file_name, 'w') as f :
+        for n, i in res.items():
+            f.write(''.join([str(n), ' ', i, '\n']))
